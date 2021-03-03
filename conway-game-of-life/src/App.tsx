3dc8995f416c7407296307a322a3fcb992a22856
ThomasGrc <47837271+ThomasGrc@users.noreply.github.com>
2021-03-03 05:27:20 +0100

    ADD - gol

---
+++ conway-game-of-life/src/App.tsx
@@ -0,0 +1,102 @@
+import React, { useCallback, useState, useRef } from 'react';
+import './App.css';
+import produce from 'immer'
+
+const numRows = 50;
+const numCols = 50;
+
+const operations = [
+  [0, 1],
+  [0, -1],
+  [1, -1],
+  [-1, 1],
+  [1, 1],
+  [-1, -1],
+  [1, 0],
+  [-1, 0]
+];
+
+const App = () => {
+
+  const [grid, setGrid] = useState(() => {
+    const rows = [];
+    for (let i = 0; i < numRows; i++) {
+      rows.push(Array.from(Array(numCols), () => 0));
+    }
+    return rows;
+  });
+
+  const [running, setRunning] = useState(false);
+
+  const runningRef = useRef(running);
+  runningRef.current = running;
+
+  const runSimulation = useCallback(() => {
+    if (!runningRef.current) {
+      return;
+    }
+
+    setGrid((g) => {
+      return produce(g, gridCopy => {
+        for (let i = 0; i < numRows; i++) {
+          for (let k = 0; k < numCols; k++) {
+            let neighbours = 0;
+            operations.forEach(([x, y]) => {
+              const newI = i + x;
+              const newK = k + y;
+              if (newI >= 0 && newI < numRows && newK >= 0 && newK < numCols) {
+                neighbours += g[newI][newK];
+              }
+            })
+
+            if (neighbours < 2 || neighbours > 3) {
+              gridCopy[i][k] = 0;
+            } else if (g[i][k] === 0 && neighbours === 3) {
+              gridCopy[i][k] = 1;
+            }
+          }
+        }
+      });
+    });
+    setTimeout(runSimulation, 1000);
+  }, [])
+
+  return (
+    <>
+      <button
+        onClick={() => {
+          setRunning(!running);
+          runningRef.current = true;
+          runSimulation();
+        }}
+      >
+        {running ? "stop" : "start"}</button>
+      <div style={{
+        display: 'grid',
+        gridTemplateColumns: `repeat(${numCols}, 20px)`
+      }}>
+        {grid.map((rows, i) =>
+          rows.map((col, k) => (
+            <div
+            key={`${i}-${k}`}
+            onClick={() => {
+              const newGrid = produce(grid, gridCopy => {
+                gridCopy[i][k] = grid[i][k] ? 0 : 1;
+              });
+              setGrid(newGrid)
+            }}
+              style={{
+                width: 20, 
+                height: 20,
+                backgroundColor: grid[i][k] ? 'black' : undefined,
+                border: "solid 1px black",
+              }}
+            />
+          ))
+        )}
+      </div>  
+    </>
+  );
+}
+
+export default App;
